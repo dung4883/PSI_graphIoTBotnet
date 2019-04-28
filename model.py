52a9ad0ca8b0df4503baebe63cc62552a233f193
dung4883 <quocdung.ngo@gmail.com>
2019-04-28 07:28:19 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TextCNN(nn.Module):
+    def __init__(self):
+        super(TextCNN, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(1, 256, kernel_size=7, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=7, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
+        self.fc = nn.Linear(8704, 2)
+
+    def forward(self, x, num):
+        x = x.view(num, 1, 1024)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return F.log_softmax(x)
